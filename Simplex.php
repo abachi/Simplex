c84d6a514f5faae47f2323b66d5f9f78a742cb41
Abachi Nasser <abachinasser@gmail.com>
2015-12-21 15:39:58 +0000

    Add method to check if the constraints are standardized

--- Simplex.php
+++ Simplex.php
@@ -5,6 +5,55 @@
  */
 class Simplex
 {
+
+	/**
+	* pattern to check if the constrants are sdandarized
+	* @constant 
+	*/
+	 const STANDARD_PATTREN = '//';
+
+	/**
+	* objective function coefficients
+	* @var array
+	*/
+	private $obj_function;
+	/**
+	* constraints coefficients
+	* @var array
+	*/
+	private $constraints;
+	/**
+	* optimization action `MAXIMISATION` OR `MINIMISATION`
+	* @var string
+	*/
+	private $opt_action;
+	/**
+	* standars form
+	* @var array
+	*/
+	private $standard_form;
+
+	public function __construct($obj_function, $constraints, $opt_action = 'max')
+	{
+		$this->obj_function = $obj_function;	
+		$this->constraints = $constraints;	
+		$this->opt_action = $opt_action;	
+	}
    
+   /**
+   * Check if the constraints are standardized
+   *
+   * @return boolean
+   */
+   public function isStandardized()
+   {	
+   		foreach( $this->constraints as $constraint){
+   			$equal_pos = sizeof($constraint) - 2;
+  			if($constraint[$equal_pos] !== '=')
+  					return false;
+   		}
+   		return true;
+   }
+
 }
 
